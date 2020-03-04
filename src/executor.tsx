8c36d8311c4c5b931166bf8b7eb0993f9dca93aa
Troy Li <troy.lty@antfin.com>
2020-03-04 15:55:58 +0800

    fix: React 16.13 warnings

--- src/executor.tsx
+++ src/executor.tsx
@@ -1,11 +1,27 @@
 import { ModelHook } from "./types";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useMemo, useRef } from "react";
 
 export function Executor<T>(props: {
   hook: ModelHook<T>;
   onUpdate: (data: T) => void;
 }) {
   const data = props.hook();
-  props.onUpdate(data);
+  const initialLoad = useRef(false);
+
+  useMemo(() => {
+    // notify the initial value
+    props.onUpdate(data);
+    initialLoad.current = false;
+  }, [])
+
+  useEffect(()=>{
+    if (initialLoad.current) {
+      // notify the following value changes
+      props.onUpdate(data);
+    } else {
+      initialLoad.current = true;
+    }
+  })
+
   return null as ReactElement;
 }
