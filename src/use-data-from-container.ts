a16f31b462890f37b4f8c6d94b4ade4693d16445
awmleer <awmleer@icloud.com>
2022-05-19 11:02:38 +0800

    refactor: reimplement `useDataFromContainer` with `useSyncExternalStore`

--- src/use-data-from-container.ts
+++ src/use-data-from-container.ts
@@ -1,43 +1,38 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import { Container } from './container'
 import { DepsFn } from './types'
+import { useSyncExternalStore } from 'use-sync-external-store/shim'
 
 export function useDataFromContainer<T>(
   container: Container<T>,
   depsFn?: DepsFn<T>
-) {
-  const [state, setState] = useState<T | undefined>(() =>
-    container ? container.data : undefined
-  )
+): T {
   const depsFnRef = useRef(depsFn)
   depsFnRef.current = depsFn
   const depsRef = useRef<unknown[]>(depsFnRef.current?.(container.data) || [])
 
-  useEffect(() => {
-    if (!container) return
-
-    function subscriber(val: T) {
-      if (!depsFnRef.current) {
-        setState(val)
-      } else {
-        const oldDeps = depsRef.current
-        const newDeps = depsFnRef.current(val)
-        if (compare(oldDeps, newDeps)) {
-          setState(val)
+  return useSyncExternalStore(
+    onStoreChange => {
+      function subscribe() {
+        if (!depsFnRef.current) {
+          onStoreChange()
+        } else {
+          const oldDeps = depsRef.current
+          const newDeps = depsFnRef.current(container.data)
+          if (compare(oldDeps, newDeps)) {
+            onStoreChange()
+          }
+          depsRef.current = newDeps
         }
-        depsRef.current = newDeps
       }
-    }
 
-    container.subscribers.add(subscriber)
-    if (container.data !== state) {
-      setState(container.data)
-    }
-    return () => {
-      container.subscribers.delete(subscriber)
-    }
-  }, [container])
-  return state!
+      container.subscribers.add(subscribe)
+      return () => {
+        container.subscribers.delete(subscribe)
+      }
+    },
+    () => container.data
+  )
 }
 
 function compare(oldDeps: unknown[], newDeps: unknown[]) {
